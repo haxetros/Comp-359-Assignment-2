1b464cbd6c67ef5373a0f0aafe03a73bbf0faee4
Wenjun（Vinna）Chen <163071958+wenjun0000@users.noreply.github.com>
2024-10-10 11:24:18 -0700

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,14 @@
+
+import java.util.Optional;
+
+public class Graph<T> {
+    private Node<T> startNode;
+
+    public Graph(Node<T> startNode) {
+        this.startNode = startNode;
+    }
+
+    public Optional<Node<T>> bfsSearch(T value, int n) {
+        return BFS.search(value, startNode, n);
+    }
+}
